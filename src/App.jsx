0245c9455378cdf1362492ede028728e0a084d8a
Khanhpisaa <khanhhnph52275@gmail.com>
2025-03-08 07:55:55 +0700

    arrow function

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,20 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
+// () => {}
+const App = () => {
   const [count, setCount] = useState(0)
 
+  // function myFunction() {
+  //   console.log("my function")
+  // }
+  // myFunction();
+
+
+  const myFunction = () => {
+    console.log("arrow function")
+  }
+  myFunction();
   return (
     <>
       <div>
@@ -16,7 +27,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Khánh Pisà</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
